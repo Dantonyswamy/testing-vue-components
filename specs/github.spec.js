929e7a975e9dfe3716d48cf8e64e0d99e69536c7
Dantony <dantonyswamy@realtermenergy.com>
2020-02-11 14:55:41 -0500

    Successfully completed methods testing

--- specs/github.spec.js
+++ specs/github.spec.js
@@ -2,5 +2,26 @@ import GithubCard from '@/github-card'
 import { mount } from '@vue/test-utils'
 
 describe('methods', () => {
+test('composeUrl', () =>{
+    const { composeUrl } = GithubCard.methods
+    expect(composeUrl(123)).toBe('https://api.github.com/users/123')    
+    })
 
+    test('fetchData', async () =>{
+        const jsonMock = jest.fn().mockResolvedValue('GITHUB DATA')
+        window.fetch = jest.fn().mockResolvedValue({
+            json:jsonMock
+        })
+        const wrapper = mount(GithubCard,{
+            methods:{
+                composeUrl:() => 'url' //this meant to be a string that will be passed to the API fetch
+            }
+        })
+        //Call the function here to test out the expected results
+        await wrapper.vm.fetchData()
+
+        expect(window.fetch).toHaveBeenCalledWith('url')
+        expect(jsonMock).toHaveBeenCalled()
+        expect(wrapper.vm.data).toBe('GITHUB DATA')
+    })
 })
